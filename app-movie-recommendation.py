0e3f5d51868b4a50867747ad9e2b33a705d99f56
harshita-313 <harshitathakur484@gmail.com>
2025-07-07 15:34:50 +0530

    Update app-movie-recommendation.py

--- app-movie-recommendation.py
+++ app-movie-recommendation.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pickle
 import pandas as pd
 import joblib 
+import gzip
 
 # Load preprocessed data
 movies = pickle.load(open('movies.pkl', 'rb')) 
-similarity = joblib.load(open('similarity.pkl', 'rb'))  
-
+with gzip.open('similarity.pkl.gz', 'rb') as f:
+    similarity = joblib.load(f)
+    
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
